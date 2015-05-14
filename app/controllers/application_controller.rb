88dab302e09bb8c9daeaec177075f190ec31e7a7
HarrietM <hsemckinnon@gmail.com>
2015-05-14 21:35:56 +1200

    Attempt heroku deploy to solve CORS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,32 +2,35 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
-  # skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
 
-  protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
-   # if: Proc.new { |c| c.request.format == 'application/json' }
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
 
-  # before_filter :cors_preflight_check
-  # after_filter :cors_set_access_control_headers
+  def cors_set_access_control_headers
+     headers['Access-Control-Allow-Origin'] = '*'
+     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+     headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+     headers['Access-Control-Max-Age'] = "1728000"
+   end
 
-  # def cors_set_access_control_headers
-  #    headers['Access-Control-Allow-Origin'] = '*'
-  #    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-  #    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-  #    headers['Access-Control-Max-Age'] = "1728000"
-  #  end
+   def cors_preflight_check
+     if request.method == 'OPTIONS'
+       # headers['Access-Control-Allow-Origin'] = '*'
+       # headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+       # headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+       # headers['Access-Control-Max-Age'] = '1728000'
 
-  #  def cors_preflight_check
-  #    if request.method == 'OPTIONS'
-  #      headers['Access-Control-Allow-Origin'] = '*'
-  #      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-  #      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-  #      headers['Access-Control-Max-Age'] = '1728000'
+       headers['Access-Control-Allow-Origin'] = '*'
+       headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+       headers['Access-Control-Request-Method'] = '*'
+       headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
 
-  #      render :text => '', :content_type => 'text/plain'
-  #    end
-  #  end
+       render :text => '', :content_type => 'text/plain'
+     end
+   end
 
 
 end
--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -17,19 +17,6 @@ class StoriesController < ApplicationController
     end
   end
 
-  def show
-    render json: {{
-      lat: 41.2864603,
-      lng: 174.77623600000004
-    }, {
-      lat: 51.5073509,
-      lng: 0.12775829999998223
-    }, {
-      lat: 30,
-      lng: 100
-    }}
-  end
-
   private
 
   def story_params
