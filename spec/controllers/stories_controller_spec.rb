81d1ba244238db4b5e659c7ad01afc6187feb3e3
Cara Hill <chilly23@gmail.com>
2015-05-15 10:33:57 +1200

    Added another test to the stories create action.

--- spec/controllers/stories_controller_spec.rb
+++ spec/controllers/stories_controller_spec.rb
@@ -11,9 +11,16 @@ RSpec.describe StoriesController, type: :controller do
       should route(:post, '/stories').to(action: :create)
     end
 
-    it "should create new story" do
+    before do
       post :create, {story: {title: "Supernatural Winnipeg", origin_latitude: -41.2967128, origin_longitude: 174.77388449999998, contribution_limit: 12}}
+    end
+
+    it "should create new story" do
       expect(Story.last.title).to eq("Supernatural Winnipeg")
+    end
+
+    it "should return an HTTP response of 200 if successful" do
+      expect(response.status).to eq(200)
     end
 
   end
