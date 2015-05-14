a0994805391e62845858062fca947c1d0ae54738
Cara Hill <chilly23@gmail.com>
2015-05-14 19:23:15 +1200

    CSRF error gone due to adding if statement to application_controller line 6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+
+  skip_before_filter :verify_authenticity_token
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
   before_filter :cors_preflight_check
   after_filter :cors_set_access_control_headers
