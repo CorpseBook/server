54ee0cca01373fe90cba289289f650f52bf6b058
Piet Geursen <pietgeursen@gmail.com>
2015-05-15 11:14:34 +1200

    Add Location model and migration. Make story have one loaction through polymorphic association.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,3 +1,4 @@
 class Story < ActiveRecord::Base
   has_many :contributions
+  has_one :location, :as => :locatable  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514053116) do
+ActiveRecord::Schema.define(version: 20150514230218) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.decimal  "lat"
+    t.decimal  "lng"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stories", force: :cascade do |t|
     t.string   "title"
