d0d950359044796bdf75b2a37b783d6797aa117a
Piet Geursen <pietgeursen@gmail.com>
2015-05-15 15:56:51 +1200

    Tidy story model location spec.

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,5 +1,5 @@
 class Story < ActiveRecord::Base
   has_many :contributions
-  has_one :location, :as => :locatable  # also works for belongs_to associations
-  acts_as_mappable :through => :location
+  has_one :location, as: :locatable  # also works for belongs_to associations
+  acts_as_mappable through: :location
 end
--- spec/models/story_spec.rb
+++ spec/models/story_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Story, type: :model do
-  describe "Story is locatable" do
-  		before(:each) do
-  			akl = FactoryGirl.create(:auckland)
-  			@welly = FactoryGirl.create(:wellington)
-  			nelly = FactoryGirl.create(:nelson)
-  			@akl_story = Story.create(location: akl)
-  			@welly_story = Story.create(location: @welly)
-  			@nelly_story = Story.create(location: nelly)
-  		end
 
-  		it "it can be created and sorted by location" do
-  			expect(Location.farthest(origin: @welly_story.location).first).to eq(@akl_story.location)
-  			# expect(Story.farthest(origin: @welly_story).first).to eq(@akl_story)
-  	  	end
+	before(:each) do
+		akl = FactoryGirl.create(:auckland)
+		@welly = FactoryGirl.create(:wellington)
+		nelly = FactoryGirl.create(:nelson)
+		@akl_story = Story.create(location: akl)
+		@welly_story = Story.create(location: @welly)
+		@nelly_story = Story.create(location: nelly)
+	end
 
-  	  	after(:each) do
-  	  		Story.delete_all
-  	  		Location.delete_all
-  	  	end
-  end
+	it "Stories can be created and sorted by location" do
+		expect(Location.farthest(origin: @welly_story.location).first).to eq(@akl_story.location)
+  	end
+
+  	after(:each) do
+  		Story.delete_all
+  		Location.delete_all
+  	end
+  
 
 end
