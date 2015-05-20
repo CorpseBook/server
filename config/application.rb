3dd40d58f121ae299d4400790d6e2282588575ba
HarrietM <apprentice@StMac4.local>
2015-05-20 15:12:02 +1200

    Add logging to Heroku.

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module Corpsebook
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.logger = Logger.new(STDOUT)
+
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
