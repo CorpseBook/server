23e38828057a856259fb236db6b7a0933395609d
HarrietM <apprentice@StMac4.local>
2015-05-20 13:56:13 +1200

    Create Story now saves location with address of lat + lng

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,16 @@
 class Location < ActiveRecord::Base
+  include Geokit::Geocoders
 
   belongs_to :locatable, :polymorphic => true
   acts_as_mappable :default_units => :kms, :lat_column_name => :lat, :lng_column_name => :lng
+
+  before_save :reverse_geocode
+
+  def reverse_geocode
+    response = Geokit::Geocoders::GoogleGeocoder.reverse_geocode([self.lat, self.lng])
+    self.address = response.full_address
+  end
+
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516035734) do
+ActiveRecord::Schema.define(version: 20150520000709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150516035734) do
     t.string   "locatable_type"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "address"
   end
 
   add_index "locations", ["locatable_type", "locatable_id"], name: "index_locations_on_locatable_type_and_locatable_id", using: :btree
@@ -43,6 +44,14 @@ ActiveRecord::Schema.define(version: 20150516035734) do
     t.boolean  "completed",          default: false
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "auth_token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "contributions", "stories"
